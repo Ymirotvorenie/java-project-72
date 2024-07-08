e43d8cfde3053736451c9e3fdbe360a9c613ea38
ymirotvorenie <karoline.navarra@gmail.com>
2024-07-08 15:18:50 +0700

    init project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ymirotvorenie/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ymirotvorenie/java-project-72/actions)
+
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/78da7cf0b2a7b7149828/maintainability)](https://codeclimate.com/github/Ymirotvorenie/java-project-72/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/78da7cf0b2a7b7149828/test_coverage)](https://codeclimate.com/github/Ymirotvorenie/java-project-72/test_coverage)
---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //JUnit
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //Lombok
+    compileOnly ("org.projectlombok:lombok:1.18.32")
+    annotationProcessor ("org.projectlombok:lombok:1.18.32")
+    testCompileOnly ("org.projectlombok:lombok:1.18.32")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.32")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
+}
